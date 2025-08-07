de2d4e7de9464685850aad1c34255ffd507e305c
orris baker <bakerorris0@gmail.com>
2025-08-07 11:05:23 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 //
 // Scripts
 // 
+            function template(row, cart) {
+  row.find('.item_name').text(cart.name);
+  row.find('.item_qty').text(cart.qty);
+  row.find('.item_total').text(cart.total);
+  return row;
+}
+
+$(document).ready(function() {
+  var newRow = $('#cart .template').clone().removeClass('template');
+  var cartItem = {
+    name: 'Glendatronix',
+    qty: 1,
+    total: 450
+  };
+  template(newRow, cartItem)
+    .appendTo('#cart')
+    .fadeIn();  
+});
 
             $(function () {
                 alert('Welcome to StarTracker! Now no longer under police investigation!');
