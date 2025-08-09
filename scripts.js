fb1b26814b8c485c6c8c9402a26ae42d5a0bae74
orris baker <bakerorris0@gmail.com>
2025-08-08 21:19:00 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Scripts
-// modifying_content
+// text_vs_html
 $(document).ready(function(){
   $('p').html('<strong>Warning!</strong> Text has been replaced for your safety.');
   $('h2').text('<strong>Warning!</strong> Title elements can be hazardous to your health.');
