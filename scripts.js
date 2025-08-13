7928a5314d678c8c1d00756a78826fc172f8843e
orris baker <bakerorris0@gmail.com>
2025-08-12 22:10:08 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
 // Scripts
+// Animating CSS Properties 
+$(document).ready(function(){
+  $('p').animate({ 
+    padding: '20px',
+    fontSize: '30px'
+  }, 2000);
+});
+
+
 
 // text_vs_html
 /*
