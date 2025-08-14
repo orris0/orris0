67f34a5bc5a7ce4381e44331febd41ba837691a3
orris baker <bakerorris0@gmail.com>
2025-08-14 16:10:28 -0400

    commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 //-----window.load start here
+
+//hover_highlight
+$(document).ready(function(){
+  $('#celebs tbody tr:even').addClass('zebra');
+  $('#celebs tbody tr').mouseover(function(){
+    $(this).addClass('zebraHover');
+  });
+  $('#celebs tbody tr').mouseout(function(){
+    $(this).removeClass('zebraHover');
+  });
+});
+
 // hover action
 $(document).ready(function(){
   $('#celebs tbody tr:even').addClass('zebra');
