ae257f1e4188da82fa5a439650de1b497d2be0ed
orris baker <bakerorris0@gmail.com>
2025-08-20 13:04:29 -0400

    commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 //-----window.load start here
 
 //-----01_animating_css
+/*
 $(document).ready(function(){
   $('p').animate({ 
     padding: '20px',
     fontSize: '30px'
   }, 2000);
 });
-
+*/
 //hover_highlight
 $(document).ready(function(){
   $('#celebs tbody tr:even').addClass('zebra');
