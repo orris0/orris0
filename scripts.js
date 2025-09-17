90424d7c512f0c8c83fe06696270b0cacc75b17a
orris baker <bakerorris0@gmail.com>
2025-09-17 00:43:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,3 +26,9 @@ $(function(){
 
 });
 
+$(document).ready(function() {
+  $('p:first')
+    .effect('shake', {times:3}, 300)
+    .effect('highlight', {}, 3000)
+    .hide('explode', {}, 1000);  
+});
