dc3f9778605fe2c9b2b4eabf4cccbdf6b0349d99
orris baker <bakerorris0@gmail.com>
2025-09-21 00:07:31 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,49 @@
+$(document).ready(function(){
+  $('a.lightbox').click(function(e) {
+    $('body').css('overflow-y', 'hidden'); // hide scrollbars!
+    
+    $('<div id="overlay"></div>')
+      .css('top', $(document).scrollTop())
+      .css('opacity', '0')
+      .animate({'opacity': '0.5'}, 'slow')
+      .appendTo('body');
+      
+    $('<div id="lightbox"></div>')
+      .hide()
+      .appendTo('body');
+      
+    $('<img>')
+      .attr('src', $(this).attr('href'))
+      .load(function() {
+        positionLightboxImage();
+      })
+      .click(function() {
+        removeLightbox();
+      })
+      .appendTo('#lightbox');
+    
+    return false;
+  });
+});
+
+function positionLightboxImage() {
+  var top = ($(window).height() - $('#lightbox').height()) / 2;
+  var left = ($(window).width() - $('#lightbox').width()) / 2;
+  $('#lightbox')
+    .css({
+      'top': top + $(document).scrollTop(),
+      'left': left
+    })
+    .fadeIn();
+}
+
+function removeLightbox() {
+  $('#overlay, #lightbox')
+    .fadeOut('slow', function() {
+      $(this).remove();
+      $('body').css('overflow-y', 'auto'); // show scrollbars!
+    });
+}
 $(function(){
   //alert('Welcome to StarTrackr! Now no longer under police investigation!');
   //alert($('#celebs tbody tr:even').length + ' elements!');
@@ -32,3 +78,4 @@ $(document).ready(function() {
     .effect('highlight', {}, 3000)
     .hide('explode', {}, 1000);  
 });
+
