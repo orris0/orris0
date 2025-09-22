dc41fbcac86a5922ec266388b3951ad95794b15a
orris baker <bakerorris0@gmail.com>
2025-09-21 20:29:42 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,11 +32,11 @@ $(document).ready(function(){
 
 // document ready
 $(function(){
-  //alert('Welcome to StarTrackr! Now no longer under police investigation!');
-  //alert($('#celebs tbody tr:even').length + ' elements!');
- //$('#celebs tbody  tr:even').addClass('zebra');
+ alert('Welcome to StarTrackr! Now no longer under police investigation!');
+ alert($('#celebs tbody tr:even').length + ' elements!');
+ $('#celebs tbody  tr:even').addClass('zebra');
   
-  /*$('<input type="button" value="toggle"  id="toggleButton">').insertBefore('#disclaimer');
+  $('<input type="button" value="toggle"  id="toggleButton">').insertBefore('#disclaimer');
   $('#toggleButton').click(function() {
     $('#disclaimer').slideToggle('slow',function() {
       $('#toggleButton').fadeOut();
@@ -44,7 +44,7 @@ $(function(){
   });
   
   $('<strong>START!</strong>').prependTo('#disclaimer');
-  $('<strong>END!</strong>').appendTo('#disclaimer');*/
+  $('<strong>END!</strong>').appendTo('#disclaimer');
   
   $('#no-script').remove();
   
@@ -58,32 +58,6 @@ $(function(){
 
 });
 
-// $(function(){
-//  alert('Welcome to StarTrackr! Now no longer under police investigation!');
-//  alert($('#celebs tbody tr:even').length + ' elements!');
-//  $('#celebs tbody  tr:even').addClass('zebra');
-  
-//  $('<input type="button" value="toggle"  id="toggleButton">').insertBefore('#disclaimer');
-//  $('#toggleButton').click(function() {
-//    $('#disclaimer').slideToggle('slow',function() {
-//      $('#toggleButton').fadeOut();
-//    });
-//  });
-  
-//  $('<strong>START!</strong>').prependTo('#disclaimer');
-//  $('<strong>END!</strong>').appendTo('#disclaimer');
-  
-//  $('#no-script').remove();
-  
-//  $('#celebs tr').mouseover(function() {
-//    $(this).addClass('zebraHover');
-//  });
-  
-//  $('#celebs tr').mouseover(function() {
-//    $(this).removeClass('zebraHover');
-//  });
-
-// });
 // Animate loader off screen
 $(window).on('load', function () {
 	"use strict";
@@ -1029,6 +1003,7 @@ new Plyr(".audio-player")
 
 
 	
+
 
 
 
