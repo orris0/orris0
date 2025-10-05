f45a08f1161a6ee16a9ce0e0ef9d6fb75212a835
orris baker <bakerorris0@gmail.com>
2025-10-05 13:11:29 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
+$(function(){
+  //alert('Welcome to StarTrackr! Now no longer under police investigation!');
+  //alert($('#celebs tbody tr:even').length + ' elements!');
+ //$('#celebs tbody  tr:even').addClass('zebra');
+  
+  /*$('<input type="button" value="toggle"  id="toggleButton">').insertBefore('#disclaimer');
+  $('#toggleButton').click(function() {
+    $('#disclaimer').slideToggle('slow',function() {
+      $('#toggleButton').fadeOut();
+    });
+  });
+  
+  $('<strong>START!</strong>').prependTo('#disclaimer');
+  $('<strong>END!</strong>').appendTo('#disclaimer');*/
+  
+  $('#no-script').remove();
+  
+  $('#celebs tr').mouseover(function() {
+    $(this).addClass('zebraHover');
+  });
+  
+  $('#celebs tr').mouseover(function() {
+    $(this).removeClass('zebraHover');
+  });
+
+});
+
 $(document).ready(function(){
   alert($('#celebs tbody tr:even').length + ' elements!');
 });
@@ -139,4 +166,5 @@ gallery.init = function() {
       gallery.slide();
     });
 }
+
 
