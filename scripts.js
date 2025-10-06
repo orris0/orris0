7449833ae40c6903c43042fcf7497d01e7cf1f2a
orris baker <bakerorris0@gmail.com>
2025-10-06 15:54:12 -0400

    commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 
+// 06_other_easing_options
+$(document).ready(function(){
+  $('p:first').animate({height: '+=300px'}, 2000, 'easeOutBounce');
+  $('p:first').animate({height: '-=300px'}, 2000, 'easeInOutExpo');
+  $('p:first').animate({height: 'hide'}, 2000, 'easeOutCirc');
+  $('p:first').animate({height: 'show'}, 2000, 'easeOutElastic');
+});
 // 05_easing
 $(document).ready(function(){
   $('p:first').toggle(function() {
