b268502e9b8894fca5c21c1ae04bf2dfc761a0c2
orris baker <bakerorris0@gmail.com>
2025-10-06 16:06:46 -0400

    commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,44 @@
 
-// 07_08_animation_queue
+// 09_animated_navigation
+$(document).ready( function () {
+    $('<div id="navigation_blob"></div>').css({
+      width: 0,
+      height: $('#navigation li:first a').height() + 10
+    }).appendTo('#navigation');
+
+    $('#navigation a').hover(
+      function () {// Mouse over function
+        $('#navigation_blob').animate(
+          {
+            width: $(this).width() + 10, 
+            left: $(this).position().left
+          },
+          {
+            duration: 'slow', 
+            easing: 'easeOutElastic',
+            queue: false
+          }
+        );
+      }, function() {// Mouse out function
+        $('#navigation_blob').animate(
+          {
+            width: $(this).width() + 10, 
+            left: $(this).position().left
+           },
+           {
+             duration:'slow', 
+             easing: 'easeOutCirc', 
+             queue: false
+           })
+            .animate(
+              {
+                left: $('#navigation li:first a').position().left
+              }, 'fast' 
+            );
+        }
+    );
+});
+// 08_animation_queue
 $(document).ready(function(){
   $('p:first').animate(
     {
