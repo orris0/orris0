0aa4f9e3883d7ba3fa42dc74440d15839681b288
orris baker <bakerorris0@gmail.com>
2025-10-06 15:56:42 -0400

    commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-
+// 07_bouncy_content_panes
+$(document).ready(function(){
+  // hide all the content panes when the page loads
+  $('#bio > div').hide();
+  
+  // uncomment the next line if you'd like the first pane to be visible by default
+  // $('#bio > div:first').show();
+  
+  $('#bio h3').click(function() {
+    $(this).next().animate({
+      'height':'toggle'
+    }, 'slow', 'easeOutBounce');
+  });
+});
 // 06_other_easing_options
 $(document).ready(function(){
   $('p:first').animate({height: '+=300px'}, 2000, 'easeOutBounce');
