c0905cfa9857443528a57c867259a8d05b947fc3
orris baker <bakerorris0@gmail.com>
2025-11-04 22:03:27 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,55 @@
 //
 // Scripts
 //
+
+// lightbox
+$(document).ready(function(){
+  $('a.lightbox').click(function(e) {
+    $('body').css('overflow-y', 'hidden'); // hide scrollbars!
+    
+    $('<div id="overlay"></div>')
+      .css('top', $(document).scrollTop())
+      .css('opacity', '0')
+      .animate({'opacity': '0.5'}, 'slow')
+      .appendTo('body');
+      
+    $('<div id="lightbox"></div>')
+      .hide()
+      .appendTo('body');
+      
+    $('<img>')
+      .attr('src', $(this).attr('href'))
+      .load(function() {
+        positionLightboxImage();
+      })
+      .click(function() {
+        removeLightbox();
+      })
+      .appendTo('#lightbox');
+    
+    return false;
+  });
+});
+
+function positionLightboxImage() {
+  var top = ($(window).height() - $('#lightbox').height()) / 2;
+  var left = ($(window).width() - $('#lightbox').width()) / 2;
+  $('#lightbox')
+    .css({
+      'top': top + $(document).scrollTop(),
+      'left': left
+    })
+    .fadeIn();
+}
+
+function removeLightbox() {
+  $('#overlay, #lightbox')
+    .fadeOut('slow', function() {
+      $(this).remove();
+      $('body').css('overflow-y', 'auto'); // show scrollbars!
+    });
+}
+
 $(document).ready(function(){
   $('#splitter > div:first').resizable({ 
     handles: 's', 
@@ -14,10 +63,6 @@ $(document).ready(function(){
     }
   });
 });
-
-
-
-
 
 // 20_resizable_textarea
 $(document).ready(function(){
