6a7cda9ede6d1dbb5a76c864f02c3f171881d96a
orris baker <bakerorris0@gmail.com>
2025-11-05 21:08:56 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 //
 // Scripts
 //
+$(document).ready(function(){
+  var jcrop = $.Jcrop('#mofat',{
+    setSelect: [10,10,300,350],
+    minSize:[50,50],
+    onChange: function(coords) {
+      // use the coordinates
+    },
+    onSelect: function(coords) {
+      // use the coordinates
+    }
+  });
+  
+  $('#crop :button').click(function() {
+    var selection = jcrop.tellSelect();
+    alert('selected size: ' + selection.w + 'x' + selection.h);
+  })
+});
+
+
+
 
 // lightbox
 $(document).ready(function(){
