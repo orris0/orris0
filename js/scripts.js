162ddd0d13c8f4385d3bec902e519e42fef4faeb
orris baker <bakerorris0@gmail.com>
2025-11-06 21:46:29 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 //
 // Scripts
 //
+$(document).ready(function(){
+  slideShow();
+});
+
+function slideShow() {
+  var current = $('#photos .show');
+  var next = current.next().length ? current.next() : current.siblings().first();
+  
+  current.hide().removeClass('show');
+  next.fadeIn().addClass('show');
+  
+  setTimeout(slideShow, 3000);
+}
+//
 $(document).ready(function() {
   // move the green box with setInterval
   var $green = $('#green');
