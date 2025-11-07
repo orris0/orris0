ec25620793c909ddc09fcc24094fc1ac7af68e6a
orris baker <bakerorris0@gmail.com>
2025-11-06 22:08:12 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,15 @@
 // Scripts
 //
 $(document).ready(function() {
+  $('#news ul').innerfade({
+    animationtype: 'slide',
+    speed: 750,
+    timeout: 3000,
+    type: 'random'
+  });
+});
+//
+$(document).ready(function() {
   rotatePics(1);
 });
 
