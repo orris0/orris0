5d1b7f8494fb3ee466a96cb0a782c9971a6f94c4
orris baker <bakerorris0@gmail.com>
2025-12-08 21:24:53 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -87,6 +87,33 @@ $(document).ready(function(){
       .slideToggle();
   });
 });
+$(document).ready(function(){
+  $('#celebs ul > li ul')
+    .click(function(event){
+      event.stopPropagation();
+    })
+    .filter(':not(:first)')
+    .hide();
+    
+  $('#celebs ul > li').click(function(){
+    var selfClick = $(this).find('ul:first').is(':visible');
+    if(selfClick) {
+      return;
+    }
+    $(this)
+      .parent()
+      .find('> li ul:visible')
+      .slideToggle();
+    
+    $(this)
+      .find('ul:first')
+      .stop(true, true)
+      .slideToggle();
+  });
+});
+
+
+
 
 
 
