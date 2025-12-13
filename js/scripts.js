b042f89bf9a490816a6443beb9c2f6b03a3151d3
orris baker <bakerorris0@gmail.com>
2025-12-13 16:34:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// 07_slideshow_cross_fade
+$(document).ready(function() {
+  rotatePics(1);
+});
+
+function rotatePics(currentPhoto) {
+  var numberOfPhotos = $('#photos img').length;
+  currentPhoto = currentPhoto % numberOfPhotos;
+	
+  $('#photos img').eq(currentPhoto).fadeOut(function() {
+    // re-order the z-index
+    $('#photos img').each(function(i) {
+      $(this).css(
+        'zIndex', ((numberOfPhotos - i) + currentPhoto) % numberOfPhotos
+      );
+    });
+    $(this).show();
+    setTimeout(function() {rotatePics(++currentPhoto);}, 4000);
+  });
+}
+
+
+
 // 06_slideshow_fade
 $(document).ready(function(){
   slideShow();
