d6be9fce30ed95722196a1ea361e9a1bf0a9a84b
orris baker <bakerorris0@gmail.com>
2025-12-13 16:48:13 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 // jquery.innerfade.js
+$(document).ready(function() {
+  $('#news ul').innerfade({
+    animationtype: 'slide',
+    speed: 750,
+    timeout: 3000,
+    type: 'random'
+  });
+});
 /* =========================================================
 
 // jquery.innerfade.js
