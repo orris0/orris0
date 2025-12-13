a735f90f265ce86db60a311d748a89e81acffbe9
orris baker <bakerorris0@gmail.com>
2025-12-13 16:13:13 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+// chapter_04/05_timers
+$(document).ready(function() {
+  // move the green box with setInterval
+  var $green = $('#green');
+    greenLeft = $green.offset().left;
+  setInterval(function() {
+    $green.css('left', ++greenLeft);
+  }, 200);
+
+    
+  // move the red box with setTimeout
+  var $red = $('#red'),
+    redLeft = $('#red').offset().left;
+  function moveRed() {
+    $red.css('left', ++redLeft);
+    setTimeout(moveRed, 200);
+  }
+  moveRed();
+});
+
+
+
 // 04_rollover_fade
 $(document).ready(function() {
   $('#fader').hover(function() {
