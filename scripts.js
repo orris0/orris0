125cb72f18f464d14c9e630d12a98027bd55552c
orris baker <bakerorris0@gmail.com>
2025-12-29 20:58:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
+// timers 
+$(document).ready(function() {
+  // move the green box with setInterval
+  var $green = $('#green');
+    greenLeft = $green.offset().left;
+  setInterval(function() {
+    $green.css('left', ++greenLeft);
+  }, 200);
+
+    
+  // move the red box with setTimeout
+  var $red = $('#red'),
+    redLeft = $('#red').offset().left;
+  function moveRed() {
+    $red.css('left', ++redLeft);
+    setTimeout(moveRed, 200);
+  }
+  moveRed();
+});
+
+
 // Jcrop
 $(document).ready(function(){
   var jcrop = $.Jcrop('#mofat',{
@@ -662,6 +683,7 @@ gallery.init = function() {
       gallery.slide();
     });
 }
+
 
 
 
