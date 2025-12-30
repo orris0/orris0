b138f235ce3598ca1d9a3fc2587df35684410af0
orris baker <bakerorris0@gmail.com>
2025-12-29 21:13:41 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+// slideshow f
+$(document).ready(function() {
+  rotatePics(1);
+});
+
+function rotatePics(currentPhoto) {
+  var numberOfPhotos = $('#photos img').length;
+  currentPhoto = currentPhoto % numberOfPhotos;
+	
+  $('#photos img').eq(currentPhoto).fadeOut(function() {
+    // re-order the z-index
+    $('#photos img').each(function(i) {
+      $(this).css(
+        'zIndex', ((numberOfPhotos - i) + currentPhoto) % numberOfPhotos
+      );
+    });
+    $(this).show();
+    setTimeout(function() {rotatePics(++currentPhoto);}, 4000);
+  });
+}
+
+
 // slideshow
 $(document).ready(function(){
   slideShow();
@@ -698,6 +720,7 @@ gallery.init = function() {
       gallery.slide();
     });
 }
+
 
 
 
