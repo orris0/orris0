2fa243a36d081c4ca71e149add94671dbaab0891
orris baker <bakerorris0@gmail.com>
2026-01-04 15:36:06 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,48 @@
+// Example 3-3. Generic error handler
+function onError(err) {
+var msg = 'Error: ';
+switch (err.code) {
+case FileError.NOT_FOUND_ERR:
+msg += 'File or directory not found';
+break;
+case FileError.SECURITY_ERR:
+msg += 'Insecure or disallowed operation';
+break;
+case FileError.ABORT_ERR:
+msg += 'Operation aborted';
+break;
+case FileError.NOT_READABLE_ERR:
+msg += 'File or directory not readable';
+break;
+case FileError.ENCODING_ERR:
+msg += 'Invalid encoding';
+break;
+case FileError.NO_MODIFICATION_ALLOWED_ERR:
+msg += 'Cannot modify file or directory';
+break;
+case FileError.INVALID_STATE_ERR:
+msg += 'Invalid state';
+break;
+case FileError.SYNTAX_ERR:
+msg += 'Invalid line-ending specifier';
+break;
+case FileError.INVALID_MODIFICATION_ERR:
+msg += 'Invalid modification';
+break;
+case FileError.QUOTA_EXCEEDED_ERR:
+msg += 'Storage quota exceeded';
+break;
+case FileError.TYPE_MISMATCH_ERR:
+msg += 'Invalid filetype';
+break;
+case FileError.PATH_EXISTS_ERR:
+msg += 'File or directory already exists at specified path';
+break;
+default:
+
+
+
+
 // Example 3-2. Requesting a filesystem with persistent storage
 const SIZE = 5*1024*1024; /*5MB*/
 const TYPE = PERSISTENT;
@@ -859,6 +904,7 @@ gallery.init = function() {
       gallery.slide();
     });
 }
+
 
 
 
