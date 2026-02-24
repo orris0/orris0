8f6de9ec50deca5b2e294fbf74e2863951d439ba
orris baker <bakerorris0@gmail.com>
2026-02-24 12:56:20 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+$('body').append('<header> </header>');
+
 $(function(){
     //alert('Welcome to StarTrackr! Now no longer under police investigation!');
       //alert($('#celebs tbody tr:even').length + ' elements!');
