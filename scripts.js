6c6f8d778680292e1436d4f154b3362acb26de5e
orris baker <bakerorris0@gmail.com>
2026-04-06 01:04:59 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 $(function(){
-  alert('Welcome to StarTrackr! Now no longer under police investigation!');
-  alert($('#celebs tbody tr:even').length + ' elements!');
- $('#celebs tbody  tr:even').addClass('zebra');
+  //alert('Welcome to StarTrackr! Now no longer under police investigation!');
+  //alert($('#celebs tbody tr:even').length + ' elements!');
+ //$('#celebs tbody  tr:even').addClass('zebra');
 
   $('<input type="button" value="toggle"  id="toggleButton">').insertBefore('#disclaimer');
   $('#toggleButton').click(function() {
@@ -9,6 +9,23 @@ $(function(){
       $('#toggleButton').fadeOut();
     });
   });
+
+  $('<strong>START!</strong>').prependTo('#disclaimer');
+  $('<strong>END!</strong>').appendTo('#disclaimer');*/
+
+  $('#no-script').remove();
+
+  $('#celebs tr').mouseover(function() {
+    $(this).addClass('zebraHover');
+  });
+
+  $('#celebs tr').mouseover(function() {
+    $(this).removeClass('zebraHover');
+  });
+
+});
+
+// Ch2-01
 $(document).ready(function() {
   alert('Welcome to StarTrackr! Now no longer under police investigation!);
 });
